3ec4e8a37b517fa0ea85425d1b78f8d98df6840a
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-11-05 18:06:55 -0700

    add table headers to Table component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Axios from 'axios'
 
+// import Search from './components/Search';
+import Table from './components/Table';
+// import Pagination from './components/Pagination';
+
 
 function App() {
   return (
-
+    <div className='container'>
+      {/* <Search /> */}
+      <Table />
+      {/* <Pagination /> */}
+    </div>
   );
 }
 
---
+++ src/components/Table.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Table = () => {
+    return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Birth Date</th>
+                        <th>Height</th>
+                        <th>Mass</th>
+                        <th>Homeword</th>
+                        <th>Species</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    )
+}
+
+export default Table;
