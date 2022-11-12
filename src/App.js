49276161ce2c9b1ac5d46f7c89f18b80f155650e
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-11-12 12:22:06 -0800

    add setIsLoading useEffect

--- src/App.js
+++ src/App.js
@@ -11,15 +11,22 @@ import Pagination from './components/Pagination';
 const App = () => {
 
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect (() => {
-    const url = "";
+    const url = "https://swapi.dev/api/people/?page=";
     fetch(url)
       .then ((response) => response.json())
       .then ((json) => setData(json['results']))
       .catch ((error) => console.log(error))
   }, []);
+
+  useEffect(() => {
+    if (data.length !== 0) {
+      setIsLoading(false);
+    }
+    console.log(data);
+  }, [data]);
 
 
   return (
