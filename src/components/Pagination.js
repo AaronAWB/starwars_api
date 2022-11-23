21e0eb19128483b846c4a95dd814a84d37eaf519
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-11-22 16:44:19 -0800

    change button styling

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,10 +4,10 @@ const Pagination = ({handlePageChange, firstPage, lastPage}) => {
     return (
         <div class="d-flex justify-content-center">
             <div class="btn-group me-2" role="group" aria-label="First group">
-                <button onClick={() => handlePageChange('Previous')} disabled={firstPage ? true : false} type="button" class="btn">Previous</button>
+                <button onClick={() => handlePageChange('Previous')} disabled={firstPage ? true : false} type="button" class="btn">Previous Page</button>
             </div>
             <div class="btn-group" role="group" aria-label="Third group">
-                <button onClick={() => handlePageChange('Next')} disabled={lastPage ? true : false} type="button" class="btn">Next</button>
+                <button onClick={() => handlePageChange('Next')} disabled={lastPage ? true : false} type="button" class="btn">Next Page</button>
             </div>
         </div>
     )
